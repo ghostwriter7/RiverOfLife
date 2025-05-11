cb01e12cf7d5a7964f67199c45631dbfd3141b12
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-05-11 18:29:02 +0200

    Added links to menu.

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,13 +1,21 @@
 import { Component, Signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router'
 import { MenuService } from '../../services/menu/menu.service'
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  protected readonly menuLinks = [
+    { label: 'New Stream', route: '/new' },
+    { label: 'Streams', route: '/streams' }
+  ];
   protected readonly isActive: Signal<boolean>;
 
   constructor(private readonly menuService: MenuService) {

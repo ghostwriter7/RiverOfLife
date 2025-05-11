3f223f953ce32e86572488d17f25ead210e8cc93
ghostwriter7 <hubert.rozplochowski@gmail.com>
2025-05-11 18:48:44 +0200

    New Stream form created.

--- src/app/pages/new-stream-page/new-stream/new-stream.component.ts
+++ src/app/pages/new-stream-page/new-stream/new-stream.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms'
 import { PageHeaderComponent } from '../../../components/page-header/page-header.component'
 
 @Component({
   selector: 'app-new-stream',
   imports: [
-    PageHeaderComponent
+    PageHeaderComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './new-stream.component.html',
   styleUrl: './new-stream.component.css'
 })
 export class NewStreamComponent {
+  protected readonly formGroup = new FormGroup({
+    title: new FormControl<string | null>(null),
+    description: new FormControl<string | null>(null),
+    category: new FormControl<string | null>(null),
+  });
 
+  protected onSubmit(): void {
+
+  }
 }
